59bfc6fa8e8e1e3c68202fac2e7a731c2e9f63de
Lucas dos Anjos <lucas.anjospro@gmail.com>
2024-04-25 14:36:37 -0300

    ajustes1

--- src/components/QrScan.jsx
+++ src/components/QrScan.jsx
@@ -1,27 +1,35 @@
 import { useState } from 'react';
 import { QrReader } from '@otterscan/react-qr-reader';
 
-export const QrScan = (props) => {
+export const QrScan = () => {
   const [data, setData] = useState('No result');
+
+  const constraints = {
+    // width: { min: 1440, ideal: 1920, max: 1920 },
+    // height: { min: 960, ideal: 1080, max: 1080 },
+    // aspectRatio: 16 / 9,
+    facingMode: "environment"
+  };
 
   return (
     <div className='QrScan'>
 
       <QrReader
         onResult={(result, error) => {
-          if(!!result) {
+          if(result) {
             setData(result?.text);
           }
 
-          if(!!error) {
+          if(error) {
             console.info(error);
           }
         }}
         style={{ width: '100%' }}
+        constraints={constraints}
       />
 
       <p>{data}</p>
-      
+
     </div>
   );
 };
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,4 @@
 // Funcionalidades / Libs:
-// import { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
 
 // Components:
 // import { Cookie } from '../../components/Cookie/cookie';
@@ -14,21 +12,13 @@ import { QrScan } from "../../components/QrScan";
 
 
 export default function Home() {
-
-
-    const constraints = {
-        width: { min: 1440, ideal: 1920, max: 1920 },
-        height: { min: 960, ideal: 1080, max: 1080 },
-        aspectRatio: 16 / 9,
-        facingMode: "environment"
-    };
     
 
     return (
         <main className='Home'>
             <h1>Teste QR Code Reader</h1>
 
-            <QrScan constraints={constraints}/>
+            <QrScan />
         </main>
     )
 }
